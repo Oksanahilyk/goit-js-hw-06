ce4adf9021d85b6698022eadd9d5c25e4dcee4c1
Oksanahilyk <hilyk@ukr.net>
2023-02-26 09:05:54 +0200

    add task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,22 @@
-const counterValue = 0;
+const buttonDescrement = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const buttonIncrement = document.querySelector(
+  'button[data-action="increment"]'
+);
+const span = document.querySelector("#value");
+const wrapper = document.querySelector("#counter");
+
+let counterValue = 0;
+const increment = () => {
+  counterValue += 1;
+  span.textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue -= 1;
+  span.textContent = counterValue;
+};
+
+buttonIncrement.addEventListener("click", increment);
+buttonDescrement.addEventListener("click", decrement);
