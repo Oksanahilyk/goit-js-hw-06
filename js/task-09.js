d5e045a0df91af39e30c4e027c69cff14c32fb02
Oksanahilyk <hilyk@ukr.net>
2023-02-26 18:37:04 +0200

    add task09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.body
+const button = document.querySelector(".change-color");
+const spun = document.querySelector(".color");
+
+button.addEventListener('click', colorChange);
+
+function colorChange(event) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spun.textContent = color;
+}
