ce6609fc012fc8da3d9f08e762fbd8713a062b3f
Oksanahilyk <hilyk@ukr.net>
2023-02-27 09:20:59 +0200

    add task02

--- js/task-02.js
+++ js/task-02.js
@@ -7,29 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
+const items = ingredients.map((element) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.classList.add("item");
+  ingredientItem.textContent = element;
+  return ingredientItem;
+}
+)
+
 const list = document.querySelector('#ingredients');
-const item1 = document.createElement("li");
-item1.classList.add("item");
-item1.textContent = 'Potatoes';
-
-const item2 = document.createElement("li");
-item2.classList.add("item");
-item2.textContent = 'Mushrooms';
-
-const item3 = document.createElement("li");
-item3.classList.add("item");
-item3.textContent = 'Garlic';
-
-const item4 = document.createElement("li");
-item4.classList.add("item");
-item4.textContent = 'Tomatos';
-
-const item5 = document.createElement("li");
-item5.classList.add("item");
-item5.textContent = 'Herbs';
-
-const item6 = document.createElement("li");
-item6.classList.add("item");
-item6.textContent = 'Condiments';
-
-list.append(item1, item2, item3, item4, item5, item6);
+list.append(...items);
