8508a0cee1c4ae62e888222e7c6729fd0f4f0605
Oksanahilyk <hilyk@ukr.net>
2023-02-27 13:11:18 +0200

    add task06

--- js/task-06.js
+++ js/task-06.js
@@ -6,9 +6,11 @@ function validCountSymbol(event) {
 	// console.log(event.currentTarget.value.length);
 	if(event.currentTarget.value.length === Number(input.dataset.length)) {
 		input.classList.add("valid");
+		input.classList.remove("invalid");
 		
 	} else {
 		input.classList.add("invalid");
+		input.classList.remove("valid");
 	}
 	
 }
